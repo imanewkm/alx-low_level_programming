1ee7b1efe5611aaa7cd0b7e6a30ee7aa77a31685
imanewkm <imanewkm@gmail.com>
2023-10-05 12:49:43 -0700

    4-puts.c betty checked

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
-/* uses only puts
- * prints "Programming is like building a multilingual puzzle"
- * returns 0
+/**
+ * main - Entry point
+ *
+ * Description: 'print a string'
+ *
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	puts("\"Programmming is like building a multilingual puzzle");
 
 	getchar();
-	return 0;
+	return (0);
 }
