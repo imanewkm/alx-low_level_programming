d6284f2242e69482ae501a32aee02598cd666b89
imanewkm <imanewkm@gmail.com>
2023-10-05 13:57:35 -0700

    101-quote intial trial

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * description: 'print a string and return 1'
+ *
+ * Return: 1 (failure)
+ */
+
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+
+	return (1);
+}
