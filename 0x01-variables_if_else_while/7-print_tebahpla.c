e6f09c13157c462edfc998e0a5af2252f24ce21b
imanewkm <imanewkm@gmail.com>
2023-10-06 14:03:08 -0700

    7 reverse alphabet:

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'reverse of the alphabet'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabet = 'z';
+
+	while (alphabet >= 'a')
+	{
+		putchar (alphabet);
+		alphabet--;
+	}
+
+	putchar ('\n');
+
+	return (0);
+}
