a207cbbd6ea6063a0cb454441d1c4defbd6a4aa0
imanewkm <imanewkm@gmail.com>
2023-10-06 12:46:44 -0700

    alphabet without e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'lowercase alphabet except q and e'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+		if (alphabet != 'q' && alphabet != 'e')
+		{
+			putchar (alphabet);
+		}
+		alphabet++;
+	}
+
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,1 +1,5 @@
-0-positive_or_negative > tells if n input is a negative or positive
+0-positive_or_negative.c > tells if n input is a negative or positive
+1-last_digit.c > last digit of an int
+2-print_alphabet.c > lowercase alphabet
+3-print_alphabet.c > lowercase and then uppercase alphabet
+4-print_alphabt.c > alphabet without q and e
