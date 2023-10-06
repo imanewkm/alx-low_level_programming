1feb1307bc0e1b548da29e8ec2465a75c49a9089
imanewkm <imanewkm@gmail.com>
2023-10-06 13:06:32 -0700

    base 10 nums from 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'base 10 numbers'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		printf("%d", n);
+		n++;
+	}
+
+	printf("\n");
+
+	return (0);
+}
