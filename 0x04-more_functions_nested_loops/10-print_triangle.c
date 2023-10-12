0dd4a0448deee0409095a263574f76274974f9d1
imanewkm <imanewkm@gmail.com>
2023-10-12 15:17:46 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -6,17 +6,25 @@
  */
 void print_triangle(int size)
 {
-	int i, j;
+	int i, j, k;
 
-	for (i = 1; i <= size; i++)
+	if (size <= 0)
 	{
-		for (j = 1; j < size; j++)
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= size; i++)
 		{
-			if ((i + j) <= size)
+			for (k = size - i; k >= 1; k--)
+			{
 				_putchar (' ');
-			else
+			}
+			for (j = 1; j <= i; j++)
+			{
 				_putchar ('#');
+			}
+			_putchar ('\n');
 		}
-		_putchar ('\n');
 	}
 }
