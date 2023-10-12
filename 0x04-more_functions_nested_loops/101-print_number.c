54d98fa36d7ab606d7b59dfaac83943f461e4cb3
imanewkm <imanewkm@gmail.com>
2023-10-12 15:52:02 -0700

    101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_number - Entry point
+ * @n: input
+ * Return: Always 0
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar (45);
+		n = -n;
+	}
+	if (n / 10)
+	{
+		print_number(n / 10);
+	}
+	_putchar ((n % 10) + '0');
+}
