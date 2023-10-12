27e9f2a677f5351171611c6068447e99fb82464e
imanewkm <imanewkm@gmail.com>
2023-10-12 14:05:31 -0700

    mistake in the 9-fizz_buzz.c code solved:

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,7 +8,7 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i <= 100; i++)
+	for (i = 1; i < 101; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 		{
@@ -18,7 +18,7 @@ int main(void)
 		{
 			printf("Fizz ");
 		}
-		else if (i % 3 == 0)
+		else if (i % 5 == 0)
 		{
 			printf("Buzz ");
 		}
