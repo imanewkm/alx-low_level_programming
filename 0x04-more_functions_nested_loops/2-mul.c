e220fa0e82f7210d566e8e7ac8305ef48540548d
imanewkm <imanewkm@gmail.com>
2023-10-12 10:39:24 -0700

    2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,11 +1,15 @@
 #include "main.h"
 /**
- * mul - Entry point
- * @a: int
- * @b: int
+ * mul - entry point
+ * @a: char
+ * @b: char
+ * @c: multiple
  * Return: 0
  */
 int mul(int a, int b)
 {
-	return (a * b);
+	int c;
+
+	c = a * b;
+	return (c);
 }
