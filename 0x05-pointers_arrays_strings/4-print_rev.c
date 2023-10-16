74101be2c78ad42d1f28052874799b749fff2806
imanewkm <imanewkm@gmail.com>
2023-10-16 13:54:04 -0700

    prints a string, in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_rev - Entry point
+ * @s: input
+ * Return: 0
+ */
+void print_rev(char *s)
+{
+	char *end = s;
+
+	while (*end)
+	{
+		end++;
+	}
+	end--;
+	while (end > s)
+	{
+		_putchar (*end);
+		end--;
+	}
+}
+
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -2,3 +2,5 @@
 1-swap.c > swaps the values of two integers
 2-strlen.c > returns the length of a string
 3-puts.c > prints a string, followed by a new line, to stdout
+4-print_rev.c > print string in reverse
+
