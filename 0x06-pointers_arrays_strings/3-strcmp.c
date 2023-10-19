b140187224a83cfdc7f5dc6542f3a1ae482d44ce
imanewkm <imanewkm@gmail.com>
2023-10-19 02:43:51 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,23 +7,10 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
-
-	while (s1[i] != '\0')
+	while (*s1 && (s1 == s2))
 	{
-		if (s2[i] == '\0')
-		{
-			return (1);
-		}
-		else if (s1[i] < s2[i])
-		{
-			return (-1);
-		}
-		else if (s1[i] > s2[i])
-		{
-			return (1);
-		}
-		i++;
+		s1++;
+		s2++;
 	}
-	return (0);
+	return (s1 - s2);
 }
