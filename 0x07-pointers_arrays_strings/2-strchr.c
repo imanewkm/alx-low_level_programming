a57b54d3f23946ab1491ca5897f1615d1d62c0eb
imanewkm <imanewkm@gmail.com>
2023-10-23 10:03:07 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strchr - locates a char in a string
+ * @s: string
+ * @c: char
+ * Return: 0
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	if (c == '\0')
+	{
+		return (s);
+	}
+	return ('\0');
+}
