475856a383391697b5f224f86485c08bdce04481
imanewkm <imanewkm@gmail.com>
2023-10-27 06:57:37 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * @argc: calculates num of input
+ * @argv: array
+ * Return: 0
+ */
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,5 @@
+0-whatsmyname.c > prints its name
+1-args.c > prints the number of arguments passed into it
+2-args.c > all arguments it receives
+3-mul.c > Multiplies two numbers
+4-add.c > adds positive numbers
