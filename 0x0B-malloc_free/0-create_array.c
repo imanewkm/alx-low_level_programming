c354910aa92cd02c6bfa2be090897e7327999d5b
imanewkm <imanewkm@gmail.com>
2023-10-30 08:38:05 -0700

    0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * create_array - array of chars, and initializes it with a specific char
+ * @size: size
+ * @c: char
+ * Return: 0
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *str;
+	unsigned int i;
+
+	str = malloc((size + 1) + sizeof(char));
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	i = 0;
+	while (i < size)
+	{
+		str[i] = c;
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,6 @@
+main.h > header file
+0-create_array.c > create array
+1-strdup.c > returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter.
+2-str_concat.c > concatenates two strings.
+3-alloc_grid.c > returns a pointer to a 2 dimensional array of integers.
+4-free_grid.c > frees a 2 dimensional grid previously created by your alloc_grid function.
