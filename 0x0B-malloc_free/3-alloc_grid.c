e8a1c0eedbe72efd57fe7da3195585e014a54c4c
imanewkm <imanewkm@gmail.com>
2023-10-30 12:20:31 -0700

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,7 +13,7 @@ int **alloc_grid(int width, int height)
 
 	s = malloc(sizeof(*s) * height);
 
-	if (width <= 0 || height <= 0 || s = 0)
+	if (width <= 0 || height <= 0 || s == 0)
 	{
 		return (NULL);
 	}
