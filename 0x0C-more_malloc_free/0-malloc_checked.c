09dfd55bad8a9f8c0416f5b65b4b9482401996d2
imanewkm <imanewkm@gmail.com>
2023-11-02 11:24:55 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,12 +3,12 @@
 #include "main.h"
 /**
  * malloc_checked - allocates memory using malloc
- * @b: int
+ * @b: unsigned int
  * Return: 0
  */
 void *malloc_checked(unsigned int b)
 {
-	void *ptr = malloc(b);
+	unsigned int *ptr = malloc(b);
 
 	if (ptr == NULL)
 	{
