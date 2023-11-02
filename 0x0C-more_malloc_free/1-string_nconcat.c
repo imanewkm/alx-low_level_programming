246eb79852496f22c5794502225bfc382c312cca
imanewkm <imanewkm@gmail.com>
2023-11-02 14:30:43 -0700

    1-string_nconcat/c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -35,11 +35,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 	if (str != NULL)
 	{
-		for (i = 0; s1[i] != '\0'; i++)
+		for (i = 0; s1[i] != '\0' && s1 != NULL; i++)
 		{
 			str[i] = s1[i];
 		}
-		for (j = 0; s2[i] != '\0' && j <= n; j++)
+		for (j = 0; s2[i] != '\0' && j < n; j++)
 		{
 			str[i + j] = s2[j];
 		}
