b84f2060f0cdc15fc3e336c879105438bf58f879
imanewkm <imanewkm@gmail.com>
2023-11-02 14:29:16 -0700

    1-string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -39,7 +39,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		{
 			str[i] = s1[i];
 		}
-		for (j = 0; s2[i] != '\0' && j < n; j++)
+		for (j = 0; s2[i] != '\0' && j <= n; j++)
 		{
 			str[i + j] = s2[j];
 		}
