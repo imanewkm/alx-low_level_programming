73aa2cd74e3028647300e2dd5ed0d0d93b0ad191
imanewkm <imanewkm@gmail.com>
2023-11-06 10:09:17 -0800

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - function
+ * @name: char
+ * @age: float
+ * @owner: char
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+dog;
+
+#endif
