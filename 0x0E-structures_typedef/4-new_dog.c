b23f17ae1d913196ee1f7f39f97eb9206c3e6f9b
imanewkm <imanewkm@gmail.com>
2023-11-06 13:53:47 -0800

    2-print_dog.c

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * new_dog - function
+ * @name: char
+ * @age: float
+ * @owner: char
+ * Return: void
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	if (d)
+	{
+
