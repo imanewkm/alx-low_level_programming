e50dba04ee4b92ae57f7d0529126fa26b55ceea9
imanewkm <imanewkm@gmail.com>
2023-11-06 14:38:10 -0800

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,39 @@
 #include "dog.h"
 #include <stdio.h>
+#include <stdlib.h>
+/**
+ * _strlen - function
+ * @str: string
+ * Return: length
+ */
+int _strlen(const char *str)
+{
+	int length = 0;
+
+	while (*str++)
+	{
+		length++;
+	}
+	return (length);
+}
+/**
+ * _strcpy - function
+ * @src: str
+ * @dest: str
+ * Return: 0
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i]; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
 /**
  * new_dog - function
  * @name: char
@@ -9,6 +43,35 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	if (d)
-	{
+	dog_t *dog;
 
+	if (!name || age < 0 || !owner)
+	{
+		return (NULL);
+	}
+
+	dog = (dog_t *) malloc(sizeof(dog_t));
+	if (dog == NULL)
+	{
+		return (NULL);
+	}
+
+	dog->name = malloc(sizeof(char) * (_strlen(name) + 1));
+	if ((*dog).name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+	dog->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+	if ((*dog).owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+	dog->name = _strcpy(dog->name, name);
+	dog->age = age;
+	dog->owner = _strcpy(dog->owner, owner);
+
+	return (dog);
+}
