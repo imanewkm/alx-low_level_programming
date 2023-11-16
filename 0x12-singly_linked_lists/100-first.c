ee76b0cd7709a8f16c3b2bc9ea2b5b5b9ffb71fe
imanewkm <110701275+imanewkm@users.noreply.github.com>
2023-11-16 14:48:23 -0800

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+/**
+ * print_before_main - prints a message before the main function is executed
+ * Return: Always 0
+ */
+void print_before_main(void) __attribute__ ((constructor));
+
+void print_before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
