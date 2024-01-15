92271fbe6196520b31f3596f01b25d3f0b88277e
imanewkm <imanewkm02@gmail.com>
2024-01-15 07:18:30 -0800

    100-operations.so

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+int add(int a, int b)
+{
+	return a + b;
+}
+int sub(int a, int b)
+{
+	return a - b;
+}
+int mul(int a, int b)
+{
+	return a * b;
+}
+int division(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error: Division by zero\n");
+		return 0;
+	}
+	return a / b;
+}
+int mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error: Division by zero\n");
+		return 0;
+	}
+	return a % b;
+}
